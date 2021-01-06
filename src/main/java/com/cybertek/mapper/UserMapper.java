af9f3b44b1fddecb5def91efdc1dbc350577ae7b
ssonata74 <ssonata@gmail.com>
2021-01-05 19:21:07 -0500

    Ticketing Project - ORM - ModelMapper - user create

--- src/main/java/com/cybertek/mapper/UserMapper.java
+++ src/main/java/com/cybertek/mapper/UserMapper.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Component;
 public class UserMapper {
     private ModelMapper modelMapper;
 
-    public UserMapper(ModelMapper modelMapper) {
+    public UserMapper(ModelMapper modelMapper) {//injection using constructor instead of @Autowire
+
         this.modelMapper = modelMapper;
     }
 
